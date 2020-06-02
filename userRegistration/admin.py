0d713dc2f80f1ec85d7c9b91b56d847ef3665017
Shreyas Mahajan <shreyashpm@gmail.com>
2020-06-02 16:33:15 +0530

    modified import-export

--- userRegistration/admin.py
+++ userRegistration/admin.py
@@ -4,29 +4,29 @@ from import_export.admin import ImportExportModelAdmin
 # Register your models here.
 from userRegistration.models import UserPersonalInfo,NewBook,Calc,WorkShopUniForm,File
 
-admin.site.register(UserPersonalInfo)
-admin.site.register(NewBook)
-admin.site.register(Calc)
-admin.site.register(WorkShopUniForm)
-admin.site.register(File)
+# admin.site.register(UserPersonalInfo)
+# admin.site.register(NewBook)
+# admin.site.register(Calc)
+# admin.site.register(WorkShopUniForm)
+# admin.site.register(File)
 
 
-# @admin.register(File)
-# class Vehicle(ImportExportModelAdmin):
-#     pass
+@admin.register(File)
+class Vehicle(ImportExportModelAdmin):
+    pass
 
-# @admin.register(WorkShopUniForm)
-# class Vehicle(ImportExportModelAdmin):
-#     pass
+@admin.register(WorkShopUniForm)
+class Vehicle(ImportExportModelAdmin):
+    pass
 
-# @admin.register(Calc)
-# class Vehicle(ImportExportModelAdmin):
-#     pass
+@admin.register(Calc)
+class Vehicle(ImportExportModelAdmin):
+    pass
 
-# @admin.register(NewBook)
-# class Vehicle(ImportExportModelAdmin):
-#     pass
+@admin.register(NewBook)
+class Vehicle(ImportExportModelAdmin):
+    pass
 
-# @admin.register(UserPersonalInfo)
-# class Vehicle(ImportExportModelAdmin):
-#     pass
+@admin.register(UserPersonalInfo)
+class Vehicle(ImportExportModelAdmin):
+    pass
